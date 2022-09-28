84e2781be7dac02df8c7134a36ae1538057834e9
Dijana Jovanovic <dijanabgjovanovic@gmail.com>
2022-09-28 13:35:51 +0200

    vezbanje, dodato menjanje boje naslova

--- dom2/1-checkbox/main.js
+++ dom2/1-checkbox/main.js
@@ -1,6 +1,6 @@
 const paragraf = document.getElementById('tekst');
 //console.log(paragraf);
-
+const naslov = document.getElementById('naslov');
 //const checkbox = document.getElementById('box');
 const checkbox = document.querySelector('#box');
 //console.log(checkbox);
@@ -8,16 +8,20 @@ console.log(checkbox.checked);
 
 if(checkbox.checked) {
    paragraf.style.visibility  = 'visible';
+    naslov.style.color = "red";
 }
 else{
     paragraf.style.visibility  = 'hidden';
+    naslov.style.color = "green";
 }
 
 checkbox.addEventListener('change', function(){
     if(checkbox.checked) {
         paragraf.style.visibility  = 'visible';
-     }
+        naslov.style.color = "red";
+    }
      else{
          paragraf.style.visibility  = 'hidden';
-     }
+         naslov.style.color = "green";
+        }
 });
